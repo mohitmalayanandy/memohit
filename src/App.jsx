cae6f32fdaa4e4afddc69fc18f3af31eead8cca1
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-03-19 11:04:06 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl font-extrabold bg-yellow-300 text-green-500'>App</h1>
+    </>
+  )
+}
+
+export default App
